df17b1c9cd061272563b12e974ffff3bf774e377
Worldinova <worldinovamanufacturers@gmail.com>
2022-07-12 14:38:14 +0530

    new ui added

--- app/src/main/java/com/worldinova/open/pdfreader/MainActivity.java
+++ app/src/main/java/com/worldinova/open/pdfreader/MainActivity.java
@@ -1,14 +1,24 @@
 package com.worldinova.open.pdfreader;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Settings;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -33,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
     boolean boolean_permission;
     File dir;
 
+    ImageView fab,help,search;
+
     final static int REQUEST_CODE = 333;
 
     @Override
@@ -41,14 +53,67 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init();
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        fab = findViewById(R.id.fab);
+        help = findViewById(R.id.help);
+        search = findViewById(R.id.search);
+
+        search.setOnClickListener(v -> search());
+
         fab.setOnClickListener(view -> {
             Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(Uri.parse("https://worldinova.web.app/diamond.html"));
+            intent.setData(Uri.parse("https://worldinova.web.app/diamond"));
             startActivity(intent);
         });
 
+
+        help.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://worldinova.web.app/help"));
+            startActivity(intent);
+        });
+
+
+
     }
+
+    private void search() {
+
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.layout_bottom_sheet);
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.BOTTOM);
+
+        LinearLayout search;
+        ImageButton search_box_btn;
+        EditText search_msg;
+        ImageView search_back;
+
+        search = dialog.findViewById(R.id.search);
+        search_back = dialog.findViewById(R.id.search_back);
+        search_msg = dialog.findViewById(R.id.search_msg);
+        search_box_btn = dialog.findViewById(R.id.search_box_btn);
+
+        search.setVisibility(View.VISIBLE);
+        search_back.setOnClickListener(v -> dialog.hide());
+
+
+        search_box_btn.setOnClickListener(v -> {
+
+            //current_state = "search";
+            String string_search_msg = search_msg.getText().toString();
+            obj_adapter.getFilter().filter(string_search_msg);
+            dialog.hide();
+
+
+        });
+
+
+    }
+
 
     private void init() {
 
